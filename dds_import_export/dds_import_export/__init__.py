10d953031739441eeb7347dfff2c0baf471100c5
Justin Duch <justin@duch.me>
2026-05-09 18:55:18 +1000

    fix

--- dds_import_export/dds_import_export/__init__.py
+++ dds_import_export/dds_import_export/__init__.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-_EXPECTED_PYTHON = (3, 10)
-
 
 def _deps_path():
     if sys.platform == "win32":
@@ -11,23 +9,27 @@ def _deps_path():
     return os.path.expanduser("~/.local/share/krita/python-deps")
 
 
-if sys.version_info[:2] != _EXPECTED_PYTHON:
-    from PyQt5.QtWidgets import QMessageBox
-
-    QMessageBox.warning(
-        None,
-        "DDS Import/Export",
-        f"Krita's Python version changed from {'.'.join(map(str, _EXPECTED_PYTHON))} "
-        f"to {sys.version_info[0]}.{sys.version_info[1]}.\n"
-        f"Reinstall Pillow:\n"
-        f'pip install --target="{_deps_path()}" '
-        f"--python-version={sys.version_info[0]}.{sys.version_info[1]} --only-binary=:all: Pillow",
-    )
-
 # Add persistent Pillow install to path
 _path = _deps_path()
 if _path not in sys.path:
     sys.path.insert(0, _path)
+
+try:
+    import PIL.Image  # noqa: F401
+except ImportError:
+    from PyQt5.QtWidgets import QMessageBox
+
+    py = f"{sys.version_info[0]}.{sys.version_info[1]}"
+    QMessageBox.warning(
+        None,
+        "DDS Import/Export",
+        f"Pillow could not be imported for Python {py} "
+        f"(Krita's Python version may have changed).\n"
+        f"Reinstall Pillow:\n"
+        f'pip install --target="{_path}" '
+        f"--python-version={py} --only-binary=:all: Pillow",
+    )
+    raise
 
 from .dds_import_export import DDSImportExport
 
